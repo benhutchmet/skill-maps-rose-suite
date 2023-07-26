608065abec53143899b61d81b16706edfbf29c51
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-07-26 14:10:54 +0100

    submit file for MMS for testing

---
+++ submit-all-multi-model.calc-anoms-model-mean-state.bash
@@ -0,0 +1,78 @@
+#!/bin/bash
+#
+# submit-all-multi-model.calc-anoms-model-mean-state.bash
+#
+# Script for submitting a job for calculating the anomalies from the model mean state for a given model.
+#
+# For example: submit-all-multi-model.calc-anoms-model-mean-state.bash HadGEM3-GC31-MM psl north-atlantic 2-5 DJF
+
+# Set the usage message
+USAGE_MESSAGE="Usage: submit-all-multi-model.calc-anoms-model-mean-state.bash <model> <variable> <region> <forecast-range> <season>"
+
+# Check that the correct number of arguments have been passed
+if [ $# -ne 5 ]; then
+    echo "$USAGE_MESSAGE"
+    exit 1
+fi
+
+# Extract the model, variable, region, forecast range and season
+model=$1
+variable=$2
+region=$3
+forecast_range=$4
+season=$5
+
+# Set the extractor script
+EXTRACTOR=$PWD/process_scripts/multi-model.calc-anoms-model-mean-state.bash
+
+# Make sure that cdo is loaded
+module load jaspy
+
+# If model=all, then run a for loop over all of the models
+if [ "$model" == "all" ]; then
+
+    # Set up the model list
+    echo "[INFO] Extracting data for all models: $models"
+
+    # Loop over the models
+    for model in $models; do
+
+        # Echo the model name
+        echo "[INFO] Calculating anomalies from model mean state for model: $model"
+
+        # Set up the output directory
+        # For the LOTUS outputs
+        OUTPUT_DIR="/work/scratch-nopw2/benhutch/$variable/$model/$region/years_${forecast_range}/$season/lotus-outputs"
+        mkdir -p $OUTPUT_DIR
+
+        # Echo the output directory
+        echo "[INFO] Output directory: $OUTPUT_DIR"
+
+        # Echo info for job submission
+        echo "[INFO] Submitting job for model: $model, variable: $variable, region: $region, forecast range: $forecast_range, season: $season"
+
+        # Submit the job
+        sbatch --partition=short-serial -t 5 -o $OUTPUT_DIR/$model.$variable.$region.$forecast_range.$season-model-mean-state.out -e $OUTPUT_DIR/$model.$variable.$region.$forecast_range.$season-model-mean-state.err $EXTRACTOR $model $variable $region $forecast_range $season
+
+    done
+    
+else
+
+    # Individual model case
+    echo "[INFO] Calculating anomalies from model mean state for model: $model"
+
+    # Set up the output directory
+    # For the LOTUS outputs
+    OUTPUT_DIR="/work/scratch-nopw2/benhutch/$variable/$model/$region/years_${forecast_range}/$season/lotus-outputs"
+    mkdir -p $OUTPUT_DIR
+
+    # Echo the output directory
+    echo "[INFO] Output directory: $OUTPUT_DIR"
+
+    # Echo info for job submission
+    echo "[INFO] Submitting job for model: $model, variable: $variable, region: $region, forecast range: $forecast_range, season: $season"
+
+    # Submit the job
+    sbatch --partition=short-serial -t 5 -o $OUTPUT_DIR/$model.$variable.$region.$forecast_range.$season-model-mean-state.out -e $OUTPUT_DIR/$model.$variable.$region.$forecast_range.$season-model-mean-state.err $EXTRACTOR $model $variable $region $forecast_range $season
+
+fi
