b79f48f9f5750417b19f37d9d2cb7ab507443cb3
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-08-17 11:58:15 +0100

    case statement for tos mergetime

--- submit-all-multi-model.mergetime.bash
+++ submit-all-multi-model.mergetime.bash
@@ -72,69 +72,121 @@ echo "[INFO] Extracting data for all models: $models"
     # Echo the model name
     echo "[INFO] Extracting data for model: $model"
 
-    # Set up the number of ensemble members and initialisation methods using a case statement
-    case $model in
-        BCC-CSM2-MR)
-            run=8
-            init_methods=1
-            ;;
-        MPI-ESM1-2-HR)
-            run=10
-            init_methods=1
-            ;;
-        CanESM5)
-            run=20
-            init_methods=1
-            ;;
-        CMCC-CM2-SR5)
-            run=10
-            init_methods=1
-            ;;
-        HadGEM3-GC31-MM)
-            run=10
-            init_methods=1
-            ;;
-        EC-Earth3)
-            run=10
-            init_methods=4
-            ;;
-        MRI-ESM2-0)
-            run=10
-            init_methods=1
-            ;;
-        MPI-ESM1-2-LR)
-            run=16
-            init_methods=1
-            ;;
-        FGOALS-f3-L)
-            run=9
-            init_methods=1
-            ;;
-        CNRM-ESM2-1)
-            run=10
-            init_methods=1
-            ;;
-        MIROC6)
-            run=10
-            init_methods=1
-            ;;
-        IPSL-CM6A-LR)
-            run=10
-            init_methods=1
-            ;;
-        CESM1-1-CAM5-CMIP5)
-            run=40
-            init_methods=1
-            ;;
-        NorCPM1)
-            run=10
-            init_methods=2
-            ;;
-        *)
-            echo "[ERROR] Model $model not found in dictionary"
-            exit 1
-            ;;
-    esac
+
+
+    if [ "$variable" == "tos" ]; then
+
+        # Echo that the variable is tos and that the tos case statement will be used
+        echo "[INFO] Variable is tos, using tos case statement"
+
+        # Set up the number of ensemble members and initialisation methods using a case statement
+        case $model in
+            CanESM5)
+                run=40
+                init_methods=1
+                ;;
+            HadGEM3-GC31-MM)
+                run=10
+                init_methods=1
+                ;;
+            EC-Earth3)
+                run=10
+                init_methods=2
+                ;;
+            FGOALS-f3-L)
+                run=6
+                init_methods=1
+                ;;
+            MIROC6)
+                run=10
+                init_methods=1
+                ;;
+            IPSL-CM6A-LR)
+                run=10
+                init_methods=1
+                ;;
+            CESM1-1-CAM5-CMIP5)
+                run=40
+                init_methods=1
+                ;;
+            NorCPM1)
+                run=10
+                init_methods=2
+                ;;
+            *)
+                echo "[ERROR] Model $model not found in dictionary"
+                exit 1
+                ;;
+        esac
+    
+    else
+        # echo that the variable is not tos and that the standard case statement will be used
+        echo "[INFO] Variable is not tos, using standard case statement"
+        
+        # Set up the number of ensemble members and initialisation methods using a case statement
+        case $model in
+            BCC-CSM2-MR)
+                run=8
+                init_methods=1
+                ;;
+            MPI-ESM1-2-HR)
+                run=10
+                init_methods=1
+                ;;
+            CanESM5)
+                run=20
+                init_methods=1
+                ;;
+            CMCC-CM2-SR5)
+                run=10
+                init_methods=1
+                ;;
+            HadGEM3-GC31-MM)
+                run=10
+                init_methods=1
+                ;;
+            EC-Earth3)
+                run=10
+                init_methods=4
+                ;;
+            MRI-ESM2-0)
+                run=10
+                init_methods=1
+                ;;
+            MPI-ESM1-2-LR)
+                run=16
+                init_methods=1
+                ;;
+            FGOALS-f3-L)
+                run=9
+                init_methods=1
+                ;;
+            CNRM-ESM2-1)
+                run=10
+                init_methods=1
+                ;;
+            MIROC6)
+                run=10
+                init_methods=1
+                ;;
+            IPSL-CM6A-LR)
+                run=10
+                init_methods=1
+                ;;
+            CESM1-1-CAM5-CMIP5)
+                run=40
+                init_methods=1
+                ;;
+            NorCPM1)
+                run=10
+                init_methods=2
+                ;;
+            *)
+                echo "[ERROR] Model $model not found in dictionary"
+                exit 1
+                ;;
+        esac
+    fi
 
     # set the output directory
     OUTPUTS_DIR="${scratch_path}/${variable}/${model}/${region}/years_${forecast_range}/${season}/lotus-outputs"
@@ -181,119 +233,168 @@ echo "[INFO] Extracting data for all models: $models"
     done
 else
 
-# Echo the model name
-# In the case of an individual model
-echo "[INFO] Extracting data for model: $model"
+    # Echo the model name
+    # In the case of an individual model
+    echo "[INFO] Extracting data for model: $model"
 
-# Set up the output directory
-OUTPUTS_DIR="${scratch_path}/${variable}/${model}/${region}/years_${forecast_range}/${season}/lotus-outputs"
-# Make the output directory
-mkdir -p $OUTPUTS_DIR
+    # Set up the output directory
+    OUTPUTS_DIR="${scratch_path}/${variable}/${model}/${region}/years_${forecast_range}/${season}/lotus-outputs"
+    # Make the output directory
+    mkdir -p $OUTPUTS_DIR
 
-# Statement for merging the data
-echo "[INFO] Merging data for model: $model"
+    # Statement for merging the data
+    echo "[INFO] Merging data for model: $model"
 
-# Set up the case statement for the number of ensemble members and initialisation methods
-# Set up the number of ensemble members and initialisation methods using a case statement
-case $model in
-    BCC-CSM2-MR)
-        run=8
-        init_methods=1
-        ;;
-    MPI-ESM1-2-HR)
-        run=10
-        init_methods=1
-        ;;
-    CanESM5)
-        run=20
-        init_methods=1
-        ;;
-    CMCC-CM2-SR5)
-        run=10
-        init_methods=1
-        ;;
-    HadGEM3-GC31-MM)
-        run=10
-        init_methods=1
-        ;;
-    EC-Earth3)
-        run=10
-        init_methods=4
-        ;;
-    MRI-ESM2-0)
-        run=10
-        init_methods=1
-        ;;
-    MPI-ESM1-2-LR)
-        run=16
-        init_methods=1
-        ;;
-    FGOALS-f3-L)
-        run=9
-        init_methods=1
-        ;;
-    CNRM-ESM2-1)
-        run=10
-        init_methods=1
-        ;;
-    MIROC6)
-        run=10
-        init_methods=1
-        ;;
-    IPSL-CM6A-LR)
-        run=10
-        init_methods=1
-        ;;
-    CESM1-1-CAM5-CMIP5)
-        run=40
-        init_methods=1
-        ;;
-    NorCPM1)
-        run=10
-        init_methods=2
-        ;;
-    *)
-        echo "[ERROR] Model $model not found in dictionary"
-        exit 1
-        ;;
-esac
+    if [ "$variable" == "tos" ]; then
 
-# loop over the ensemble members
-for run in $(seq 1 $run); do
+            # Echo that the variable is tos and that the tos case statement will be used
+            echo "[INFO] Variable is tos, using tos case statement"
 
-    # if init_methods is greater than 1, then loop over the init methods
-    if [ $init_methods -gt 1 ]; then
+        # Set up the number of ensemble members and initialisation methods using a case statement
+        case $model in
+            CanESM5)
+                run=40
+                init_methods=1
+                ;;
+            HadGEM3-GC31-MM)
+                run=10
+                init_methods=1
+                ;;
+            EC-Earth3)
+                run=10
+                init_methods=2
+                ;;
+            FGOALS-f3-L)
+                run=6
+                init_methods=1
+                ;;
+            MIROC6)
+                run=10
+                init_methods=1
+                ;;
+            IPSL-CM6A-LR)
+                run=10
+                init_methods=1
+                ;;
+            CESM1-1-CAM5-CMIP5)
+                run=40
+                init_methods=1
+                ;;
+            NorCPM1)
+                run=10
+                init_methods=2
+                ;;
+            *)
+                echo "[ERROR] Model $model not found in dictionary"
+                exit 1
+                ;;
+        esac
 
-        # loop over the init methods
-        for init in $(seq 1 $init_methods); do
+    else
+        # echo that the variable is not tos and that the standard case statement will be used
+        echo "[INFO] Variable is not tos, using standard case statement"
+        
+        # Set up the number of ensemble members and initialisation methods using a case statement
+        case $model in
+            BCC-CSM2-MR)
+                run=8
+                init_methods=1
+                ;;
+            MPI-ESM1-2-HR)
+                run=10
+                init_methods=1
+                ;;
+            CanESM5)
+                run=20
+                init_methods=1
+                ;;
+            CMCC-CM2-SR5)
+                run=10
+                init_methods=1
+                ;;
+            HadGEM3-GC31-MM)
+                run=10
+                init_methods=1
+                ;;
+            EC-Earth3)
+                run=10
+                init_methods=4
+                ;;
+            MRI-ESM2-0)
+                run=10
+                init_methods=1
+                ;;
+            MPI-ESM1-2-LR)
+                run=16
+                init_methods=1
+                ;;
+            FGOALS-f3-L)
+                run=9
+                init_methods=1
+                ;;
+            CNRM-ESM2-1)
+                run=10
+                init_methods=1
+                ;;
+            MIROC6)
+                run=10
+                init_methods=1
+                ;;
+            IPSL-CM6A-LR)
+                run=10
+                init_methods=1
+                ;;
+            CESM1-1-CAM5-CMIP5)
+                run=40
+                init_methods=1
+                ;;
+            NorCPM1)
+                run=10
+                init_methods=2
+                ;;
+            *)
+                echo "[ERROR] Model $model not found in dictionary"
+                exit 1
+                ;;
+        esac
+    fi
 
-            # echo the init method
-            echo "[INFO] Initialisation method: $init"
+    # loop over the ensemble members
+    for run in $(seq 1 $run); do
+
+        # if init_methods is greater than 1, then loop over the init methods
+        if [ $init_methods -gt 1 ]; then
+
+            # loop over the init methods
+            for init in $(seq 1 $init_methods); do
+
+                # echo the init method
+                echo "[INFO] Initialisation method: $init"
+
+                # set the output file name
+                OUTPUT_FILE="${OUTPUTS_DIR}/${model}_r${run}_i${init}_year_${forecast_range}_${season}.nc"
+
+                # echo the output file name
+                echo "[INFO] Output file name: $OUTPUT_FILE"
+
+                # submit the job to LOTUS
+                sbatch --partition=short-serial -t 5 -o $OUTPUTS_DIR/merge.${model}.${run}.${init}.${forecast_range}.${season}.out -e $OUTPUTS_DIR/merge.${model}.${run}.${init}.${forecast_range}.${season}.err $EXTRACTOR $model $variable $region $forecast_range $season $run $init
+                
+
+            done
+
+        # otherwise, just run the single init method
+        else
 
             # set the output file name
-            OUTPUT_FILE="${OUTPUTS_DIR}/${model}_r${run}_i${init}_year_${forecast_range}_${season}.nc"
+            OUTPUT_FILE="${OUTPUTS_DIR}/${model}_${run}_${init_method}_year_${forecast_range}_${season}.nc"
 
             # echo the output file name
             echo "[INFO] Output file name: $OUTPUT_FILE"
 
             # submit the job to LOTUS
-            sbatch --partition=short-serial -t 5 -o $OUTPUTS_DIR/merge.${model}.${run}.${init}.${forecast_range}.${season}.out -e $OUTPUTS_DIR/merge.${model}.${run}.${init}.${forecast_range}.${season}.err $EXTRACTOR $model $variable $region $forecast_range $season $run $init
-            
+            sbatch --partition=short-serial -t 5 -o $OUTPUTS_DIR/merge.${model}.${run}.${init_methods}.${forecast_range}.${season}.out -e $OUTPUTS_DIR/merge.${model}.${run}.${init_methods}.${forecast_range}.${season}.err $EXTRACTOR $model $variable $region $forecast_range $season $run $init_methods
 
-        done
-
-    # otherwise, just run the single init method
-    else
-
-        # set the output file name
-        OUTPUT_FILE="${OUTPUTS_DIR}/${model}_${run}_${init_method}_year_${forecast_range}_${season}.nc"
-
-        # echo the output file name
-        echo "[INFO] Output file name: $OUTPUT_FILE"
-
-        # submit the job to LOTUS
-        sbatch --partition=short-serial -t 5 -o $OUTPUTS_DIR/merge.${model}.${run}.${init_methods}.${forecast_range}.${season}.out -e $OUTPUTS_DIR/merge.${model}.${run}.${init_methods}.${forecast_range}.${season}.err $EXTRACTOR $model $variable $region $forecast_range $season $run $init_methods
-
-    fi
-done
+        fi
+    done
 fi
