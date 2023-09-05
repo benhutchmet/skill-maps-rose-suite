abb158e5d8c1503888c42f98f042bce4027dbdc6
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-09-05 16:43:41 +0100

    updating for u and v fields

--- process_scripts/multi-model.sel-region-forecast-range-season.bash
+++ process_scripts/multi-model.sel-region-forecast-range-season.bash
@@ -531,6 +531,64 @@ elif [ "$variable" == "tos" ]; then
         echo "[ERROR] Model not recognised for variable tos"
         exit 1
     fi
+
+# If the variable is ua or va
+# TODO: update this to include call for ua models
+elif [ "$variable" == "ua" ]; then
+    # Set up the single file models
+    # which have been downloaded into my gws from ESGF
+    if [ "$model" == "NorCPM1" ] || [ "$model" == "IPSL-CM6A-LR" ] || [ "$model" == "MIROC6" ] || [ "$model" == "MPI-ESM1-2-HR" ] || [ "$model" == "CanESM5" ] || [ "$model" == "CMCC-CM2-SR5" ] || [ "$model" == "CESM1-1-CAM5-CMIP5" ] || [ "$model" == "FGOALS-f3-L" ] || [ "$model" == "BCC-CSM2-MR" ]; then
+        # Set up the input files from canari
+        # example: /gws/nopw/j04/canari/users/benhutch/dcppA-hindcast/ua/CanESM5/data
+        # file example: ua_Amon_MIROC6_dcppA-hindcast_s2021-r9i1p1f1_gn_202111-203112.nc
+        # specify a regular grid - gn - for CESM1-1-CAM5-CMIP5 (has both gr and gn)
+        files="${canari_base_dir}/${experiment}/${variable}/${model}/data/${variable}_Amon_${model}_${experiment}_s${year}-r${run}i*p*f*_gn_*.nc"
+    elif [ "$model" == "HadGEM3-GC31-MM" ]; then
+        # Set up the input files from badc
+        multi_files="/badc/cmip6/data/CMIP6/DCPP/$model_group/$model/${experiment}/s${year}-r${run}i?p?f?/Amon/ua/g?/files/d????????/*.nc"
+
+        # set up the merged file first
+        merged_file_dir=${canari_base_dir}/${experiment}/data/${variable}/${model}/merged_files
+        mkdir -p $merged_file_dir
+
+        # set up the start year
+        start_year="${year}11"
+        # set up the end year
+        end_year=$((year + 11))"03"
+
+        # set up the merged file name
+        merged_filename=${variable}_Amon_${model}_${experiment}_s${year}-r${run}i1p1f2_gn_${start_year}-${end_year}.nc
+        # set up the merged file path
+        merged_file_path=${merged_file_dir}/${merged_filename}
+
+        # if the merged file already exists, do not overwrite
+        if [ -f "$merged_file_path" ]; then
+            echo "INFO: Merged file already exists: $merged_file_path"
+            echo "INFO: Not overwriting $merged_file_path"
+        else
+            echo "INFO: Merged file does not exist: $merged_file_path"
+            echo "INFO: Proceeding with script"
+
+            # merge the files
+            cdo mergetime $multi_files $merged_file_path
+
+            echo "[INFO] Finished merging files for $model"
+        fi
+
+        # Set up the input files
+        files=${merged_file_path}
+
+    elif [ "$model" == "EC-Earth3" ]; then
+
+        # Set up the i1 multi files
+        i1_multi_files="/badc/cmip6/data/CMIP6/DCPP/$model_group/$model/${experiment}/s${year}-r${run}i1p?f?/Amon/ua/g?/files/d????????/*.nc"
+
+        # Set up the merged file dir
+        merged_file_dir=${canari_base_dir}/${experiment}/data/${variable}/${model}/merged_files
+        mkdir -p $merged_file_dir
+
+        # Set up the start year
+
 else
     echo "[ERROR] Variable not recognised"
     exit 1
