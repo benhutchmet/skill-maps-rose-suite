56aa64e7d8eef48587f92c11ba37b04c0ea26d4d
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-01-05 12:02:09 +0000

    update psl associative array

--- dictionaries.bash
+++ dictionaries.bash
@@ -28,8 +28,7 @@ scratch_path="/work/scratch-nopw2/benhutch"
 canari_base_dir="/gws/nopw/j04/canari/users/benhutch"
 
 # Create an associative array for the models
-declare -A psl_models_nens
-psl_models_nens=(["BCC-CSM2-MR"]=8
+declare -A psl_models_nens=(["BCC-CSM2-MR"]=8
     ["MPI-ESM1-2-HR"]=10
     ["CanESM5"]=20
     ["CMCC-CM2-SR5"]=10
